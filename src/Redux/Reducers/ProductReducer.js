5f7b4105519906ff9d7fd69ebe4adfa0cf49c9a6
AhadBawani <ahadbawani123@gmail.com>
2023-01-30 11:16:10 +0530

    30 01 changes

---
+++ src/Redux/Reducers/ProductReducer.js
@@ -0,0 +1,30 @@
+import ActionType from "../ActionType";
+const productState = {
+  Products: null,
+  seller:null,
+  ProdCategory:null,
+  seller_detail:{
+    name:"Guest",
+    photo:"",
+  },
+  query:""
+};
+
+const ProductReducer = (state = productState, { type, payload }) => {
+  switch (type) {
+    case ActionType.PRODUCTS:
+      return { ...state, Products: payload };
+    case ActionType.CATEGORY:
+      return { ...state, ProdCategory: payload };
+    case ActionType.SEARCH_QUERY:
+      return { ...state, query: payload };
+    case ActionType.SELLER:
+      return { ...state, seller: payload };
+    case ActionType.SELLER_DETAIL:
+      return { ...state, seller_detail: payload };
+    default:
+      return state;
+  }
+};
+
+export default ProductReducer;
